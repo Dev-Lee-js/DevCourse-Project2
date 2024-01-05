9c5619701e163647ef2fba7e7956df54797f7f6e
Dev-Lee-js <whdtlr202@gmail.com>
2024-01-05 15:54:45 +0900

    개별 도서 조회

--- routes/books/controller.js
+++ routes/books/controller.js
@@ -18,7 +18,47 @@ const allBooks = (req, res) => {
 
 };
 
+const bookDetail = (req, res) => {   
+
+    const {id} = req.params;
+
+    const sql = `SELECT * FROM books WHERE id=?`;    
+    
+    conn.query(sql, id, (err, results) => {
+        if (err) {
+            return res.status(StatusCodes.BAD_REQUEST).end();
+        } 
+        if(results.length){
+            return res.status(StatusCodes.OK).json(results)
+        } else {
+            return res.status(StatusCodes.NOT_FOUND).end()
+        }
+    });
+
+};
+
+const bookByCategory = (req, res) => {   
+
+    const {category_id} = req.query;
+
+    const sql = `SELECT * FROM books WHERE category_id=?`;    
+    
+    conn.query(sql, category_id, (err, results) => {
+        if (err) {
+            return res.status(StatusCodes.BAD_REQUEST).end();
+        } 
+        if(results.length){
+            return res.status(StatusCodes.OK).json(results)
+        } else {
+            return res.status(StatusCodes.NOT_FOUND).end()
+        }
+    });
+
+};  
+
 
 module.exports = {
-    allBooks
+    allBooks,
+    bookDetail,
+    bookByCategory
 }
--- routes/books/index.js
+++ routes/books/index.js
@@ -9,13 +9,10 @@ router.get('/', ctrl.allBooks );
 
 
 //개별 도서 조회
-router.get('/:id',(req,res)=>{
-    res.json("개별 도서 조회");
-})
+router.get('/:id',ctrl.bookDetail );
+
 // //카테고리별 도서 목록 조회
-// router.get('/',(req,res)=>{
-//     res.json("카테고리별 도서 목록 조회")
-// })
+router.get('/', ctrl.bookByCategory)
 
 
 module.exports = router
