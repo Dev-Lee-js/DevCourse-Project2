27ef25fb8b7d25e086f56d071554b92194fec66f
Dev-Lee-js <whdtlr202@gmail.com>
2024-01-07 23:42:55 +0900

    도서 개별 조회 조인 구현

--- routes/books/controller.js
+++ routes/books/controller.js
@@ -4,9 +4,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 
-const allBooks = (req, res) => {   
+const allBooks = (req, res) => {
 
-    const sql = `SELECT * FROM books`;    
+    const sql = `SELECT * FROM books`;
 
     conn.query(sql, (err, results) => {
         if (err) {
@@ -18,17 +18,18 @@ const allBooks = (req, res) => {
 
 };
 
-const bookDetail = (req, res) => {   
+const bookDetail = (req, res) => {
 
-    const {id} = req.params;
+    const { id } = req.params;
 
-    const sql = `SELECT * FROM books WHERE id=?`;    
-    
+    const sql = `SELECT * FROM books LEFT JOIN category
+                 ON books.category_id = category.id WHERE books.id = ?`;
+
     conn.query(sql, id, (err, results) => {
         if (err) {
             return res.status(StatusCodes.BAD_REQUEST).end();
-        } 
-        if(results.length){
+        }
+        if (results.length) {
             return res.status(StatusCodes.OK).json(results)
         } else {
             return res.status(StatusCodes.NOT_FOUND).end()
@@ -37,24 +38,24 @@ const bookDetail = (req, res) => {
 
 };
 
-const bookByCategory = (req, res) => {   
+const bookByCategory = (req, res) => {
 
-    const {category_id} = req.query;    
+    const { category_id } = req.query;
 
-    const sql = `SELECT * FROM books WHERE category_id=?`;    
-    
+    const sql = `SELECT * FROM books WHERE category_id=?`;
+
     conn.query(sql, category_id, (err, results) => {
         if (err) {
             return res.status(StatusCodes.BAD_REQUEST).end();
-        } 
-        if(results.length){
+        }
+        if (results.length) {
             return res.status(StatusCodes.OK).json(results)
         } else {
             return res.status(StatusCodes.NOT_FOUND).end()
         }
     });
 
-};  
+};
 
 
 module.exports = {
